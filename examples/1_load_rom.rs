95890a2d56748543df34a2a94b019ebd4f92b0bc
nukeop <nukeop@gumblert.tech>
2018-07-28 04:29:09 +0200

    create example

---
+++ examples/1_load_rom.rs
@@ -0,0 +1,6 @@
+extern crate mr_cool_nes;
+use mr_cool_nes::core::rom;
+
+fn main() {
+    let _rom = rom::Rom::load(&"tests/roms/cpu_dummy_reads/cpu_dummy_reads.nes".to_owned());
+}
--- tests/ppu_tests.rs
+++ tests/ppu_tests.rs
@@ -16,4 +16,52 @@ mod ppu_tests {
         assert_eq!(ppu.regs.ppu_ctrl, 0xDD);
     }
     
+    #[test]
+    fn store_byte_ppu_mask() {
+        let mut ppu = setup_ppu();
+        ppu.store_byte(0x2001, 0xDD);
+        assert_eq!(ppu.regs.ppu_mask, 0xDD);
+    }
+
+    #[test]
+    fn store_byte_ppu_status() {
+        let mut ppu = setup_ppu();
+        ppu.store_byte(0x2002, 0xDD);
+        assert_eq!(ppu.regs.ppu_status, 0x0);
+    }
+
+    #[test]
+    fn store_byte_ppu_oam_addr() {
+        let mut ppu = setup_ppu();
+        ppu.store_byte(0x2003, 0xDD);
+        assert_eq!(ppu.regs.oam_addr, 0xDD);
+    }
+
+    #[test]
+    fn store_byte_ppu_oam_data() {
+        let mut ppu = setup_ppu();
+        ppu.store_byte(0x2004, 0xDD);
+        assert_eq!(ppu.regs.oam_data, 0xDD);
+    }
+
+    #[test]
+    fn store_byte_ppu_ppu_scroll() {
+        let mut ppu = setup_ppu();
+        ppu.store_byte(0x2005, 0xDD);
+        assert_eq!(ppu.regs.ppu_scroll, 0xDD);
+    }
+
+    #[test]
+    fn store_byte_ppu_ppu_addr() {
+        let mut ppu = setup_ppu();
+        ppu.store_byte(0x2006, 0xDD);
+        assert_eq!(ppu.regs.ppu_addr, 0xDD);
+    }
+
+    #[test]
+    fn store_byte_ppu_ppu_data() {
+        let mut ppu = setup_ppu();
+        ppu.store_byte(0x2007, 0xDD);
+        assert_eq!(ppu.regs.ppu_data, 0xDD);
+    }
 }
